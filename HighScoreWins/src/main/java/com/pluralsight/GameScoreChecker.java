6c366c0885887dcb31bb20fce68b3bef77fcaa2b
hperez29 <perezherbie29@yahoo.com>
2025-04-17 15:02:37 -0400

    HighScoreWins

---
+++ HighScoreWins/src/main/java/com/pluralsight/GameScoreChecker.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class GameScoreChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt Enter the game score
+        System.out.println("Enter the game score in the format ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
